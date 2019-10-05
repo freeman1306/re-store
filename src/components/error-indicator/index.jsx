3ac906584327ecc3e91fe97f802ed7f7982b2b5d
@freeman1306 <zavalkin06@gmail.com>
2019-10-05 22:41:48 +0300

    file structure is almost done

---
+++ src/components/error-indicator/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.css";
+
+const ErrorIndicator = () => {
+	return <div>ErrorIndicator</div>;
+};
+
+export default ErrorIndicator;
---
+++ src/components/hoc/with-bookstore-service.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BookstoreServiceConsumer} from '../bookstore-service-context'
+import BookstoreService from '../../services/bookstore-service'
+
+const withBookstoreService = () => (Wrapped) => {
+
+    return (props) => {
+        return (
+            <BookstoreServiceConsumer>
+                {
+                    (bookstoreService) => {
+                   return(     <Wrapped {...props}
+                    bookstoreService={bookstoreService} />)
+                    }
+                }
+</BookstoreServiceConsumer>
+        )
+    }
+}
+
+export default withBookstoreService
